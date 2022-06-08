1df63337361938d2f0009133e3a8c2703b490083
andre <61792917+luuxis@users.noreply.github.com>
2022-06-08 12:33:46 +0200

    add code test

---
+++ test/index.js
@@ -0,0 +1,24 @@
+const { DataTypes, dataBase } = require('../index');
+let config = {
+    database: 'news',
+    fileType: 'sqlite',
+    tableName: 'news',
+    tableConfig: {
+        title: DataTypes.CHAR(200),
+        content: DataTypes.TEXT,
+        author: DataTypes.CHAR(30)
+    }
+}
+
+async function main() {
+    let { table } = await dataBase.intilize(config);
+    await dataBase.createData(table, {
+        title: 'test',
+        content: 'test',
+        author: 'test'
+    });
+    // let test = await dataBase.deleteData(table, 1);
+    // console.log(test);
+}
+
+main();
